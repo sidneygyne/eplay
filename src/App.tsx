6ad979da20ec3e59ae30422f1addcd30df9d32b9
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2025-07-06 19:08:08 -0300

    configurando o redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { Header } from './components/Header'
 import { EstiloGlobal } from './styles'
 import { Rotas } from './routes'
 import { Footer } from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <EstiloGlobal />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <EstiloGlobal />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
