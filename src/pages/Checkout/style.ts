0923ebb830940527643e9d7dffecf29b25e0bd73
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2025-07-19 18:33:21 -0300

    Checkout  - Pagamento

--- src/pages/Checkout/style.ts
+++ src/pages/Checkout/style.ts
@@ -1,13 +1,28 @@
 import styled from 'styled-components'
 import cores from '../../styles/cores'
 
-export const Row = styled.div`
+type RowProps = {
+  marginTop?: string
+}
+
+type InputGroup = {
+  maxWidth?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+export const Row = styled.div<RowProps>`
   display: flex;
+  align-items: flex-end;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroup>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     font-size: 14px;
@@ -15,11 +30,29 @@ export const InputGroup = styled.div`
     display: block;
   }
 
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     border: 1px solid ${cores.branca};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+export const TagButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
